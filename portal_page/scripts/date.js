e4f6386dcfaab227844393222d7749127191e145
Crazycritz <joshua@pdm-pix.com>
2023-01-06 13:26:33 +0200

    finished portal page

---
+++ portal_page/scripts/date.js
@@ -0,0 +1,5 @@
+let text = document.lastModified;
+
+const date = new Date(document.lastModified)
+
+document.getElementById('last-modified').innerHTML = 'The date and time of the document when it is last modified is : '+ document.lastModified;
