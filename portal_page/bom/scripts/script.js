39dd61fa21ee1418ca459192627c046ebd3b4ed8
Crazycritz <joshua@pdm-pix.com>
2023-01-31 13:29:01 +0200

    dom

---
+++ portal_page/bom/scripts/script.js
@@ -0,0 +1,24 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const ul = document.querySelector("#list");
+
+button.addEventListener("click", function () {
+  if (input.value == "") {
+    alert("There is no input");
+    return false;
+  }
+  let li = document.createElement("li");
+  li.innerHTML = input.value;
+  let buttonDel = document.createElement("button");
+  buttonDel.innerHTML = "❌";
+  li.setAttribute("id", input.value);
+  buttonDel.addEventListener( "click", function () {
+    document.getElementById(input.value).remove()
+    document.getElementById("favchap").focus();
+    input.value = '';
+  });
+
+  li.appendChild(buttonDel);
+  ul.appendChild(li);
+ 
+});
