9518d25d1e1cad791cc82fc79a65bc3c0e3211cd
Crazycritz <joshua@pdm-pix.com>
2023-03-09 10:32:10 +0200

    weather api

--- portal_page/chamber/scripts/windchill.js
+++ portal_page/chamber/scripts/windchill.js
@@ -1,16 +1,15 @@
-let temp = parseInt(document.querySelector('#temp').textContent);
-let speed = parseInt(document.querySelector('#speed').textContent);
+let tempF = parseInt(document.querySelector('#current-temp').textContent);
+let speedM = parseInt(document.querySelector('#speed').textContent);
 
 
-tempF = (temp * 2) + 30;
-speedM = speed / 1.609344 ;
+
 console.log(tempF)
 console.log(speedM)
 
 if (tempF <= 50 && speedM > 3) {
     chill = Math.floor(((35.74 + (0.6215 * tempF) - (35.75 * speedM**0.16) + (0.4275 * tempF * speedM**0.16)) -30) /2);
 
-    document.querySelector('#chill').innerHTML = 'Wind Chill: ' + chill + '°C';
+    document.querySelector('#chill').innerHTML = 'Wind Chill: ' + chill + '°F';
     console.log(chill)
 }
 else {
